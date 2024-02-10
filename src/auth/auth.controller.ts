b60bf7086756d9663e641febb27ad61db22a953c
rashmipullur <mheresince1998@gmail.com>
2024-02-10 19:34:53 +0530

    using auth dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
     console.log({
-      dto,
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
     });
     return this.authService.signup();
   }
